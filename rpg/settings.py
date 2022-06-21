b3d364fffe58eac5de523d1470602678ef09e6ca
Guilherme-Anderson <music.gui.1998@gmail.com>
2022-06-21 17:14:36 -0300

    Co-authored-by: Yas <yasrivera.dev@gmail.com> Co-authored-by: Buzzer <bxreis21@users.noreply.github.com>

--- rpg/settings.py
+++ rpg/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-59=9$et17#ey%n#w7f=^(eu_iptt8%bw@c=0@adr@fdku!$imu
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS =[]
 
 
 # Application definition
